13d4725d2fd8c7dcb99d55f4a48acc1169e0111e
shigenobu <shigenobu314@yahoo.co.jp>
2020-08-03 22:42:18 +0900

    三分固定のカウントダウンのみの実装完了

--- ShiftaDebandTimer/MainWindow.xaml.cs
+++ ShiftaDebandTimer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using HongliangSoft.Utilities.Gui;
+using System.Diagnostics;
 
 namespace ShiftaDebandTimer
 {
@@ -20,9 +23,75 @@ namespace ShiftaDebandTimer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+		int shiftaTimeSec = 0;
+		Stopwatch shiftaStopwatch = new Stopwatch();
+		int debandTimeSec = 0;
+		Stopwatch debandStopwatch = new Stopwatch();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			KeyboardHook keyboardHook = new KeyboardHook();
+			keyboardHook.KeyboardHooked += KeyboardHook_KeyboardHooked;
+
+			//タイマー設定
+			//10ミリ秒
+			dispatcherTimer.Interval = new TimeSpan(100);
+			dispatcherTimer.Tick += DispatcherTimer_Tick;
+			dispatcherTimer.Start();
+		}
+
+		private void DispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			int nokoriSec = shiftaTimeSec - (int)shiftaStopwatch.Elapsed.TotalSeconds;
+			if (nokoriSec < 0)
+			{
+				shiftaTextBlok.Text = "0:00";
+				shiftaStopwatch.Stop();
+				return;
+			}
+			shiftaTextBlok.Text = $"{nokoriSec / 60}:{nokoriSec % 60:00}";
+
+			nokoriSec = debandTimeSec - (int)debandStopwatch.Elapsed.TotalSeconds;
+			if (nokoriSec < 0)
+			{
+				debandTextBlok.Text = "0:00";
+				debandStopwatch.Stop();
+				return;
+			}
+			debandTextBlok.Text = $"{nokoriSec / 60}:{nokoriSec % 60:00}";
+		}
+
+		private void KeyboardHook_KeyboardHooked(object sender, KeyboardHookedEventArgs e)
+		{
+			if (e.KeyCode == System.Windows.Forms.Keys.D6 && e.UpDown == KeyboardUpDown.Down)
+			{
+				shiftaTimeSec = 180;
+				shiftaStopwatch.Restart();
+			}
+			if (e.KeyCode == System.Windows.Forms.Keys.D7 && e.UpDown == KeyboardUpDown.Down)
+			{
+				debandTimeSec = 180;
+				debandStopwatch.Restart();
+			}
+		}
+
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			this.DragMove();
+		}
+
+		private void MenuItem_CloseClick(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MenuItem_SettingClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("設定画面");
 		}
 	}
 }
