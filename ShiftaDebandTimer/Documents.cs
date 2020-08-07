13a0c901f41f1506fc996d97fa85727b8d7de17b
kasamatsu <kasamatsu@kasa.com>
2020-08-07 22:46:45 +0900

    Documents.cs追加

---
+++ ShiftaDebandTimer/Documents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShiftaDebandTimer
+{
+    [Serializable]
+    public class Documents
+    {
+        /// <summary>
+        /// シフタのキー
+        /// </summary>
+        public Key ShiftaKey { get; set; } = Key.D6;
+
+        /// <summary>
+        /// デバンドのキー
+        /// </summary>
+        public Key DebandKey { get; set; } = Key.D7;
+    }
+}
